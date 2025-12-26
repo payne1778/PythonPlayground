b04c529c651d0ccdf6d8af3fae1e0129ea365083
Blake Payne <56892670+payne1778@users.noreply.github.com>
2025-12-26 15:50:49 -0600

    accept Ruff suggestion

--- catacomb_crawler/actor.py
+++ catacomb_crawler/actor.py
@@ -1,5 +1,6 @@
 import random
 
+
 class Actor:
     def __init__(self, name: str, health: int, max_damage: int, x: int, y: int) -> None:
         self.name = name
@@ -24,39 +25,41 @@ class Actor:
             or (self.x == other.x and self.y == other.y + 1)
             or (self.x == other.x and self.y == other.y - 1)
         )
-        
+
     def deal_damage(self, other: "Actor") -> int:
         damage: int = random.randint(1, self.max_damage)
         other.health -= damage
-        if not other.is_alive(): other.health = 0; 
+
+        if not other.is_alive():
+            other.health = 0
         return damage
-        
+
     def move(self, direction: str, size: int) -> bool:
         actor_was_moved: bool = True
         match (direction):
-            case ("east"):
-                if self.x == size - 1: 
+            case "east":
+                if self.x == size - 1:
                     actor_was_moved = False
                 else:
                     self.x += 1
-            case ("west"):
-                if self.x == 0: 
+            case "west":
+                if self.x == 0:
                     actor_was_moved = False
                 else:
                     self.x -= 1
-            case ("south"):
-                if self.y == size - 1: 
+            case "south":
+                if self.y == size - 1:
                     actor_was_moved = False
                 else:
                     self.y += 1
-            case ("north"):
-                if self.y == 0: 
+            case "north":
+                if self.y == 0:
                     actor_was_moved = False
                 else:
                     self.y -= 1
-            case (_):
+            case _:
                 actor_was_moved = False
         return actor_was_moved
-                
+
     def __str__(self) -> str:
         return f"{self.name} at {self.x}, {self.y} with {self.health} health"
