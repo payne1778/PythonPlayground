f9d3edc87a46c09c9e9fc3a770deff2bf66ac54c
Blake Payne <56892670+payne1778@users.noreply.github.com>
2025-12-28 02:46:05 -0600

    add rounding test

---
+++ rounding/rounding.md
@@ -0,0 +1,7 @@
+# rounding
+
+Testing rounding functionality. it's like 4am don't judge pls :sob:
+
+```bash
+python rounding/rounding.py
+```
---
+++ rounding/rounding.py
@@ -0,0 +1,10 @@
+import math
+
+
+def main() -> None:
+    print(math.ceil(256 / 50))
+    print(math.ceil(131 / 100))
+
+
+if __name__ == "__main__":
+    main()
